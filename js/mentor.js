0cb172c54f2564ef39b99f335216986412d8b9ac
fatemeh jahanbini <jahanbinifatemeh@gmail.com>
2025-08-07 14:34:19 +0330

    up

--- js/mentor.js
+++ js/mentor.js
@@ -1,6 +1,9 @@
 function hidesidebar(){
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display ='none';
+    const menubutton = document.querySelector(".menubtn");
+    menubutton.style.display ='flex';
+    
 }
 function showsidebar(){
     const sidebar = document.querySelector(".sidebar");
